c8404f7ef381044c6087ae570841aa08d106da36
Vishnu Dileesh <dev@vishnudileesh.com>
2020-11-17 21:41:52 +0530

    Setup firebase and add nav bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,82 @@
 <script>
-	export let name;
+  import { FirebaseApp, User } from "sveltefire";
+
+  import firebase from "firebase/app";
+  import "firebase/firestore";
+  import "firebase/auth";
+
+  let firebaseConfig = {
+    apiKey: "AIzaSyD_pZr2ArWpEZS9HNS3wnN4YQpxyRlWrK4",
+    authDomain: "resume-craft.firebaseapp.com",
+    databaseURL: "https://resume-craft.firebaseio.com",
+    projectId: "resume-craft",
+    storageBucket: "resume-craft.appspot.com",
+    messagingSenderId: "794936095350",
+    appId: "1:794936095350:web:13cee2db43a88e7c51a557",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  function googleLogin() {
+    auth.signInWithPopup(googleProvider);
+  }
+
+  // pages
+
+  import Home from "./Home.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: #11091a;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  :global(html, body) {
+    color: #11091a;
+    font-family: "Poppins", sans-serif;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  :global(a) {
+    text-decoration: none;
+  }
+
+  :global(button) {
+    padding: 1em 1.5em;
+    border: none;
+    background-color: white;
+    border-radius: 0.5em;
+    cursor: pointer;
+  }
 </style>
+
+<svelte:head>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap"
+    rel="stylesheet" />
+</svelte:head>
+
+
+<Home on:login={googleLogin} />
+
+  <!-- <FirebaseApp {firebase}> -->
+    <!-- <User let:user let:auth> -->
+      <!-- <h1>Hello User {user.uid}</h1> -->
+<!--  -->
+      <!-- <button on:click={() => auth.signOut()}>Log out</button> -->
+<!--  -->
+      <!-- <div slot="signed-out"> -->
+        <!-- <h1>Come on, Log in</h1> -->
+<!--  -->
+        <!-- [> <button on:click={() => auth.signInWithPopup(googleProvider)}>Login with <] -->
+        <!-- [> Google</button> <] -->
+      <!-- </div> -->
+    <!-- </User> -->
+  <!-- </FirebaseApp> -->
+</main>
---
+++ src/Home.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function loginClick() {
+    dispatch("login", {});
+  }
+</script>
+
+<style>
+  main {
+    background-color: red;
+    height: 100vh;
+  }
+
+  .container {
+    margin: auto;
+    width: 80%;
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 1em 0;
+  }
+</style>
+
+<main>
+  <div class="container">
+    <nav>
+      <h1><a href="/">Resume Craft</a></h1>
+      <button on:click={loginClick}>Login with Google</button>
+    </nav>
+  </div>
+</main>
